6a98fd60fc3d02f5dc78893f596014a7aa0d65a9
giordano <giordano.cristini@gmail.com>
2016-11-12 14:35:32 +0100

    prove

--- layers.py
+++ layers.py
@@ -8,6 +8,9 @@ from keras_dt import *
 class EmbeddingDT(Layer):
     
     input_ndim = 2
+    @staticmethod
+    def to_chr(array):
+        return "".join([chr(int(i)) for i in array])
 
     def __init__(self,dt, input_dim, output_dim,
                   input_length=None
@@ -25,24 +28,28 @@ class EmbeddingDT(Layer):
     def build(self, input_shape):
     	#ci serve la matrice identita'
         self.W = K.eye(self.output_dim)
-        
-        
-   
 
     def get_output_shape_for(self, input_shape):
         if not self.input_length:
             input_length = input_shape[1]
         else:
             input_length = self.input_length
+        #print (input_shape[0], input_length, self.output_dim)
         return (input_shape[0], input_length, self.output_dim)
     
     def call(self, x, mask=None):
-
+        #print type(x)
+        #print x
         #questo serve ad evitare di calcolare il dt su un tensore
         #si verifica quando viene aggiunto il layer al modello
-
-        if type(x) != str:
+        #print type(x)
+        if str(x) == 'embeddingdt_input_1':
+            #print 'a'
             return K.zeros(1)
+        #print 'qui'
+        x = EmbeddingDT.to_chr(x.eval())
+        #print x
+        #print x
         if x not in self.cache:
         	self.cache[x] = self.dt.dt(x)
         #return self.cache[x] direttamente o con dot?
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from keras_dt import *
 from trees import *
 from layers import *
-from keras.layers import Dense
+from keras.layers import RepeatVector
 from keras.models import Sequential
 
+def to_ord(string):
+    return [ord(i) for i in string]
 trees=[]
 with open('SampleInput.dat','r') as f:
 	trees=[line.replace('\n','') for line in f.readlines()]
@@ -11,13 +13,15 @@ with open('SampleInput.dat','r') as f:
 model = Sequential()
 dt = DT(dim=4096,lexicalized=True)
 model.add(EmbeddingDT(dt,1,4096))
-#to use this embedding
-#remember to reshape the output!
-#model.add(Reshape((4096,)))
-#model.add(Dense(1,activation='sigmoid'))
+model.add(RepeatVector(1))
 model.compile(loss='mse',optimizer='sgd')
+
 #keras converte direttamente a float tutto
 #questo ci impedisce di usare il layer
-print model.predict(trees[0])
+#proviamo a codificare l'albero
+inp=np.array(to_ord(trees[0]))
+#print inp
+#print model.layers[0].call(K.variable(inp)).eval()
+print model.predict([inp])
 
 
