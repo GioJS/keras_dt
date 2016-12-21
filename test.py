bc65b0daf354364f01b34f06266dec44dcdd9424
Giordano Cristini <giordano.cristini@gmail.com>
2016-12-21 10:28:51 +0100

    tf fix

--- test.py
+++ test.py
@@ -17,15 +17,21 @@ if K.backend() == 'tensorflow':
 	sess = K.tf.Session()
 	K.set_session(sess)
 	with sess.as_default():
+		
 		model = Sequential()
-	dt = DT(dim=4096, lexicalized=True)
-	model.add(EmbeddingDT(dt, trees, 2000, 1, 4096))
-	print model.layers[0].output_shape
-	model.add(Reshape((2,2048)))
-	model.compile(loss='mse', optimizer='sgd')
-	
-	print model.layers[0].call(indices[0])
-	print model.layers[1].call(model.layers[0].call(indices[0])).eval()
+		dt = DT(dim=4096, lexicalized=True)
+		model.add(EmbeddingDT(dt, trees, 2000, 1, 4096))
+		print model.layers[0].output_shape
+		#sembra che in questo modo evito la terza dimensione
+		model.add(Reshape((4096,)))
+		model.add(Dense(4096, activation="sigmoid"))
+		model.compile(loss='mse', optimizer='sgd')
+		
+		#print model.layers[0].call(indices[0])
+		print model.layers[1].call(model.layers[0].call(indices[0])).eval()
+		#restituisce un array di 0 perche' non c'e' learning
+		print model.predict(indices)
+
 else:
 
 	model = Sequential()
