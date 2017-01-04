4eb3ccd9977bdc2260b7163391f8494cc4429a77
Giordano Cristini <giordano.cristini@gmail.com>
2017-01-04 12:52:02 +0100

    new convolution with circulant matrix

--- convolutions.py
+++ convolutions.py
@@ -21,10 +21,19 @@ def shuffled_circular_convolution(x,y,permutations=None):
 	
 	return circular_convolution(x[permutations[0]],y[permutations[1]],permutations)
 
+def permutation_matrices(N):
+	I = np.eye(N)
+	p1 = circulant(I[:,0])
+
+	return p1, p1.T
+
 def cc_circulant(x,y):
 	A = circulant(x) #circulant matrix
 	#permuation matrices
-	Phi_1 = []
-	Phi_2 = []
+	Phi1,Phi2 = permutation_matrices(x.shape[0])
 	#circular convolution
-	return Phi_1 * A * Phi_2 * y
+	#print Phi1,Phi2
+	return Phi1.dot(A).dot(Phi2).dot(y)
+x=np.array([1,2,3])
+y=np.array([2,3,4])
+print cc_circulant(x,y), circular_convolution(x,y)
