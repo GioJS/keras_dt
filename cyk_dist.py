ab0ed499b0da8011111a847e8be6827233f9f9d2
giordano <giordano.cristini@gmail.com>
2017-01-11 13:30:44 +0100

    implementing

--- cyk_dist.py
+++ cyk_dist.py
@@ -9,7 +9,8 @@ import parserNLP.CYK as CYK
 from vectors import *
 from keras_dt import *
 from convolutions import *
-gen = Vector_generator(dim=3)
+dim = 1024
+gen = Vector_generator(dim)
 
 def sc(v):
     if type(v) != np.ndarray:
@@ -18,12 +19,12 @@ def sc(v):
 
 def invsc(v):
     return sc(v).T
-
+#initialization of level 0
 def init(w):
-    P = K.zeros((len(w),len(w))).eval()
-    print P[0]
+    P = K.zeros((dim,dim)).eval()
+    #print P[0]
     for i in range(len(w)):
-        s = (sc(gen.get_random_vector('0')).dot(sc(gen.get_random_vector(str(i)))).dot(sc(gen.get_random_vector(w[i]))).dot(sc(gen.get_random_vector('Sep'))).dot(np.eye(1,len(w),0)[0]))
+        s = (sc(gen.get_random_vector('0')).dot(sc(gen.get_random_vector(str(i)))).dot(sc(gen.get_random_vector(w[i]))).dot(sc(gen.get_random_vector('Sep'))).dot(np.eye(1,dim,0)[0]))
         #print s
         P[0] = P[0] + s
     return P
@@ -39,6 +40,6 @@ G.add_rules_from_file('gramm_l')
 parser = CYK.CYK(G)
 parser.parse('a a b')
 P = parser.C
-
+print P
 P_dist=init('aab')
 print P_dist
