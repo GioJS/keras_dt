2f363bcda486fcf67c679ace86a1c1e4cfc7f2bb
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-01-18 16:40:05 +0100

    dubbi

--- cyk_dist.py
+++ cyk_dist.py
@@ -91,20 +91,22 @@ def P_to_dist(parser,w):
     for i in range(len(parser.C)):
         #preterminal trees
         #print chart
-        tree = parser.get_tree(parser.C[i,i])
-        #print tree
-        dt_tree = sc(gen.get_random_vector('0')).dot(sc(gen.get_random_vector(str(i)))).dot(sc(dt.dt(tree, to_penn=False)))
-        #print dt_tree
-        Dp = Dp + dt_tree
+        for A in parser.C[i,i]:
+            tree = parser.get_tree(A)
+            #print tree
+            dt_tree = sc(gen.get_random_vector('1')).dot(sc(gen.get_random_vector(str(i)))).dot(sc(dt.dt(tree, to_penn=False))).dot(invsc(gen.get_random_vector(str(i)))).dot(invsc(gen.get_random_vector('0'))).dot(Dp)
+            #print dt_tree
+            Dp = Dp + dt_tree
     #generic row
     #print Dp
     for i in range(2,len(w)):
         for j in range(0,len(w)-i+2):
             if i==j or not parser.C[j,i]:
                 continue
-            #print parser.C[j,i]
-            tree = parser.get_tree(parser.C[j,i])
-            Dp = Dp + sc(gen.get_random_vector(str(j))).dot(sc(gen.get_random_vector(str(i)))).dot(sc(dt.dt(tree, to_penn=False)))
+            for A in  parser.C[j,i]:
+                tree = parser.get_tree(A)
+                for k in range(0,i+1):
+                    Dp = Dp + sc(gen.get_random_vector(str(j))).dot(sc(gen.get_random_vector(str(i)))).dot(sc(dt.dt(tree, to_penn=False)))
     return Dp
 '''
 con grammatiche stupide e frasi piccole
@@ -126,9 +128,9 @@ for i in range(2,3):
         K.set_session(sess)
         with sess.as_default():
             Pd = cyk_dist(P,w)
-            #print Pd
+            print Pd
             Dp = P_to_dist(parser,w)
-            #print Dp
+            print Dp
             import scipy.spatial.distance as sp
             sim = 1 - sp.cdist(Pd,Dp,'cosine')
             print sim
