a6d30c175a0d1d19ce3260f3b7befae9531fa8bf
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-01-22 14:20:30 +0100

    fix trees

--- cyk_dist.py
+++ cyk_dist.py
@@ -112,7 +112,7 @@ def test_P(parser,w):
     		for A in parser.C[j,i]:
     			#print A
     			tree = parser.get_tree(A)
-                #print 'tree: ',tree
+                print 'tree: ',tree
                 td = sc(gen.get_random_vector(str(i))).dot(sc(gen.get_random_vector(str(j)))).dot(tree_dist(tree))
                 #print td
                 Dp = Dp + td
@@ -133,7 +133,7 @@ for i in range(2,3):
     parser.parse(w)
     P = parser.C
     print P
-    #print parser.get_tree(P[1,2][0])
+    #print parser.get_tree(P[2,2][0])
     if K.backend() == 'tensorflow':
         sess = K.tf.Session()
         K.set_session(sess)
