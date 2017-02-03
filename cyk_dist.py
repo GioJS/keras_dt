56c98a0df9f32943e580e2deae6180d408c6cce5
giordano <giordano.cristini@gmail.com>
2017-02-03 15:28:32 +0100

    check preterminals

--- cyk_dist.py
+++ cyk_dist.py
@@ -171,13 +171,13 @@ for i in range(2,3):
     #else:
     Pd = cyk_dist(G,w)
     Pd = invsc(v("0")).dot(invsc(v("1"))).dot(Pd)
-    #Pd = Pd.dot(invsc(v('Sep')))
+    Pd = Pd.dot(invsc(v('Sep')))
         # # print Pd
     Dp = test_P(parser,w)
     Dp = invsc(v("0")).dot(invsc(v("1"))).dot(Dp)
         # #print Dp
-    #print 'Pd: ',Pd[:,0].dot(sc(v('D')).dot(sc(v('a')))[:,0])
-    #print 'Dp: ',Dp[:,0].dot(sc(v('D')).dot(sc(v('a')))[:,0])
+    print 'Pd: ',Pd[:,0].dot(sc(v('D')).dot(sc(v('a')))[:,0])
+    print 'Dp: ',Dp[:,0].dot(sc(v('D')).dot(sc(v('a')))[:,0])
 
 
     print Pd[:,0].dot(Dp[:,0])
