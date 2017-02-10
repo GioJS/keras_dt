01a8037ba9f93f9a27ffd986307be46ec4100ba2
giordano <giordano.cristini@gmail.com>
2017-02-10 14:37:03 +0100

    test

--- cyk_dist.py
+++ cyk_dist.py
@@ -30,7 +30,7 @@ def preterminals(P,G,w):
     R = np.array([0])
 
     for rule in G.get_unit_productions():
-        print rule
+        #print rule
         R = R + (sc(v(rule.head())).dot(sc(v('Sep'))).dot(sc(v(rule.head()))).dot(sc(v('Sep'))).dot(sc(v(rule.production()))).dot(invsc(v('Sep'))).dot(invsc(v(rule.head())))).dot(invsc(v('Sep'))).dot(invsc(v(rule.production())))
 
     s = np.array([0])
@@ -184,12 +184,13 @@ for i in range(2,3):
     #Dw0 = sc(v('D')).dot(circulant(v('a')))
     from trees import *
     #t_d = tree_dist(Tree('D',[Tree('a',[])]))
+    print tree_dist(Tree.from_penn('(D a)')).dot(invsc(v('a'))).dot(invsc(v('Sep'))).dot(invsc(v('D')))
     t_d = sc(v("1")).dot(sc(v("1"))).dot(sc(v('D'))).dot(sc(v('Sep'))).dot(tree_dist(Tree.from_penn('(D a)'))).dot(invsc(v('Sep'))).dot(invsc(v('D')))
     #t_d = tree_dist(Tree.from_penn('(S (D a) (E b))'))
     #t_d = tree_dist(Tree.from_penn('(S (D a) (S (D a) (E b)))'))
     #print t_d[:,0].dot(Dw0[:,0])
     #print Pd[:,0].dot(t_d[:,0])
-    print Pd.dot(t_d)
+    print Pd.dot(t_d.T)
     #Pd = Pd.dot(circulant(v('a')).T)
         # # print Pd
     Dp = test_P(parser,w)
