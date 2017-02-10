4be6e96fec595fccecadac0bdda63351ddeb24e1
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-02-10 19:44:46 +0100

    test

--- cyk_dist.py
+++ cyk_dist.py
@@ -65,6 +65,7 @@ def binary(P,G,w):
         R[A] = compute_R(G, rules_A)
     for i in range(2,n):
         for j in range(1,n-i+2):
+            print i,j
             for A in G.groups:
                 Ra = R[A]
                 Pa = np.array([0])
