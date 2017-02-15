968b20167dbd455fa6f4a74ba8d558ba6dccabd1
giordano <giordano.cristini@gmail.com>
2017-02-15 16:07:36 +0100

    test b

--- cyk_dist.py
+++ cyk_dist.py
@@ -170,7 +170,7 @@ for i in range(2,3):
     #fin = S.dot(sep).dot(sep.T).dot(D.T).dot(index2.T).dot(index1.T).dot(P_12).dot(rule).dot(index3.T).dot(index1.T).dot(P_13).dot(E).dot(sep)
     #t = tree_dist(Tree.from_penn('(S (D a) (E b))'))
     t1 = S.dot(sep).dot(tree_dist(Tree.from_penn('(D a)'))).dot(Phi).dot(tree_dist(Tree.from_penn('(E b)')))
-    t2 = S.dot(sep).dot(tree_dist(Tree.from_penn('(D a)'))).dot(sep.T).dot(D.T).dot(D).dot(sep).dot(Phi).dot(tree_dist(Tree.from_penn('(E b)')))
+    t2 = S.dot(sep).dot(tree_dist(Tree.from_penn('(D a)'))).dot(sep.T).dot(D.T).dot(D).dot(sep).dot(Phi).dot(index3.T).dot(index1.T).dot(index1).dot(index3).dot(tree_dist(Tree.from_penn('(E b)')))
     #Pd = cyk_dist(G,w)
     #print Pd.dot(P_1.T)
     print t1.dot(t2.T)
