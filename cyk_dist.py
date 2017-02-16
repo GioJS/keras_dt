38a2d593abefd72ffc4315ef8ef6f1329ac87947
giordano <giordano.cristini@gmail.com>
2017-02-16 16:23:34 +0100

    from tree to cyk matrix

--- cyk_dist.py
+++ cyk_dist.py
@@ -43,7 +43,7 @@ def preterminals_simple(P,G,w):
     s = np.array([0])
     for i in range(len(w)):
         s = s + index1.dot(sc(v(str(i+1)))).dot(R).dot(invsc(v(str(i+1)))).dot(index0.T).dot(P)
-    P = s
+    P = P + s
     return P
 
 
