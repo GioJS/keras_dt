ce608c3bd75e15698fec0eb40101fc070539d9b0
giordano <giordano.cristini@gmail.com>
2017-02-16 16:57:34 +0100

    from tree to cyk matrix

--- trees.py
+++ trees.py
@@ -21,9 +21,9 @@ class Tree(list):
 		return cyk_matrix
 	def __make_nodes(self,t,cyk_matrix, displacement):
 		if len(t)==1:
-			cyk_matrix += index0.dot(sc(v(displacement))).dot(sc(v(t.label)))
+			cyk_matrix += index0.dot(sc(v(str(displacement)))).dot(sc(v(t.label)))
 		else:    
-			cyk_matrix += sc(v(t.get_nterminals()-1)).dot(displacement).dot(sc(v(t.label)))
+			cyk_matrix += sc(v(str(t.get_nterminals()-1))).dot(str(displacement)).dot(sc(v(t.label)))
 			self.__make_nodes(t[0],cyk_matrix,displacement)
 			self.__make_nodes(t[1],cyk_matrix,displacement + t[0].get_nterminals())
 		return cyk_matrix
