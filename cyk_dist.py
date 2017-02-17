1d04c6dd0ff2191e49001582dbdeff73fb7d0c11
giordano <giordano.cristini@gmail.com>
2017-02-17 15:52:37 +0100

    logistic

--- cyk_dist.py
+++ cyk_dist.py
@@ -237,10 +237,11 @@ if __name__ == '__main__':
         
         dist_P = cyk_dist_simple(G,w)
         print dist_P
-        pure_P = index1.dot(index1).dot(D)
+        '''pure_P = index1.dot(index1).dot(D)
         pure_P += index1.dot(index2).dot(D)
         pure_P += index1.dot(index3).dot(E)
-        pure_P += index2.dot(index2).dot(S)
+        pure_P += index2.dot(index2).dot(S)'''
+        print index2.T.dot(index2.T).dot(dist_P).dot(D.T)
         '''R =  D.T.dot(S.T)
         print sigmoid(index2.T.dot(index1.T).dot(pure_P).dot(R).dot(index3.T).dot(index1.T).dot(pure_P))
         R =  D.T.dot(E.T)
