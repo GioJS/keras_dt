e35d874670b6c17cad2207be25b5a057ac55a7d6
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-03-15 11:06:12 +0100

    refactoring rnn

--- layers.py
+++ layers.py
@@ -135,8 +135,8 @@ class PreterminalRNN(Recurrent):
     def step(self, x, states):
         P = states[0] #matrix P at step i-1
 
-        tmp = sigmoid(K.dot(symbol, K.dot(self.position,K.dot(K.transpose(index0),P))))
-        output =  P + K.dot(index1,K.dot(self.position,tmp))
+        tmp = sigmoid(K.dot(symbol, K.dot(self.position, K.dot(K.transpose(index0), P))))
+        output =  P + K.dot(index1, K.dot(self.position, tmp))
         self.position = K.dot(self.index1, self.position)
         return output, [output]
 
