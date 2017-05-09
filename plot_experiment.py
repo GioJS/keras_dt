3c270ba47a88f54687aa470334b44e8e1acbae50
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-05-09 19:37:34 +0200

    check sentence - no shuffle

--- plot_experiment.py
+++ plot_experiment.py
@@ -14,7 +14,7 @@ med = conf['med'] #per i plot tolgo brutti valori
 
 #mettere glob.glob!!!
 precisions_files = glob.glob("experiments/precisions_%s_*"%(name))
-recalls_files = glob.glob("experiments/precisions_%s_*"%(name))
+recalls_files = glob.glob("experiments/recalls_%s_*"%(name))
 print(precisions_files)
 print(recalls_files)
 ##togliere valori med
