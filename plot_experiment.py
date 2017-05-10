94f32676f4238f352317ec662f2282ad0d773455
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-05-10 15:24:18 +0200

    check sentence - no shuffle

--- plot_experiment.py
+++ plot_experiment.py
@@ -1,7 +1,7 @@
 import json
 import numpy as np
-# import matplotlib.pyplot as plt
-# import seaborn
+import matplotlib.pyplot as plt
+import seaborn
 import glob
 
 with open('conf.json') as f:
@@ -17,13 +17,17 @@ med = conf['med']  # per i plot tolgo brutti valori
 # plottare tali dati
 ###
 # mettere glob.glob!!!
+precisions = []
+recalls = []
 precisions_files = glob.glob("experiments/precisions_%s_*" % (name))
+# for file_p in precisions_files:
+#     precisions.append(np.loadtxt(file_p))
+# plt.plot(np.arange(15), [np.mean(i) for i in precisions])
+# plt.show()
 recalls_files = glob.glob("experiments/recalls_%s_*" % (name))
 print(precisions_files)
 print(recalls_files)
-##togliere valori med
-precisions = []
-recalls = []
+
 for file_p, file_r in zip(precisions_files, recalls_files):
     precisions.append(np.loadtxt(file_p))
     recalls.append(np.loadtxt(file_r))
