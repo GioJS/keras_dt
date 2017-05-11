52ad2fbbb8907d191ce181981e32a42eeaff4fc7
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-05-11 15:58:18 +0200

    fix, mean,var, errorbars etc

--- plot_experiment.py
+++ plot_experiment.py
@@ -17,22 +17,37 @@ med = conf['med']  # per i plot tolgo brutti valori
 # plottare tali dati
 ###
 dims = [500, 1000, 1500, 2000]
-precisions = []
-recalls = []
+precisions = {}
+recalls = {}
 precisions_files = {}
+recalls_files = {}
 for i in dims:
-    precisions_files[i] = glob.glob("experiments/precisions*_%d_m1_*" % i)
+    precisions_files[i] = glob.glob("experiments/precisions*_%d_m1*" % i)
+    recalls_files[i] = glob.glob("experiments/recalls*_%d_m1*" % i)
+for i in dims:
+    for p, r in zip(precisions_files[i], recalls_files[i]):
+        print(p)
+        precisions[i] = np.loadtxt(p)
+        recalls[i] = np.loadtxt(r)
+means_P = []
+vars_P = []
+means_R = []
+vars_R = []
+for i in [500]:
+    means_P.append(np.mean(precisions[i]))
+    vars_P.append(np.var(precisions[i]))
+    means_R.append(np.mean(recalls[i]))
+    vars_R.append(np.var(recalls[i]))
+print(vars_P)
 
-for i in dims:
-    precision_f = []
-    for f in precisions_files[i]:
-        p = np.loadtxt(f)
-        print(max(p))
-        precision_f.append(np.mean(p))
-    plt.plot(np.arange(50), precision_f)
-    plt.show()
-    # precisions.append(np.mean(precision_f))
-print(precisions)
+x = np.arange(1)
+
+plt.scatter(x, means_P)
+# plt.scatter(x, vars_P)
+plt.errorbar(x, means_P, yerr=vars_P)
+plt.show()
+# precisions.append(np.mean(precision_f))
+# print(precisions)
 # for file_p in precisions_files:
 #     precisions.append(np.loadtxt(file_p))
 # plt.plot(np.arange(15), [np.mean(i) for i in precisions])
